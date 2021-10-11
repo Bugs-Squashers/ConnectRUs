e5e2bbc722ac9de06fea731ff514b221489f8616
Amanda Chang <43429828+amandajyu@users.noreply.github.com>
2021-10-10 23:12:12 -0700

    Amanda's A6 + requirements.txt update

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 import unittest
-import main
+import requests
+
+class TestMain(unittest.TestCase):
+    URL = 'http://192.168.0.32:5000/'
+
+    # Amanda's Test
+    def test_home(self):
+        r = requests.get(self.URL)
+        # status code 200 means we got a response back
+        self.assertEquals(r.status_code, 200)
+        # since no user is logged in, the response should be "Hello World!"
+        self.assertEquals(r.text, "Hello World!")
