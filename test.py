ac3c205f126115d3707380ee06373cca73f13a51
Jonathan Halim <63839517+jahazio@users.noreply.github.com>
2021-10-12 20:34:40 -0700

    added Jonathan's test

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import unittest
 import requests
 
+from main import login
+
 
 class TestMain(unittest.TestCase):
     URL = 'http://192.168.0.32:5000/'
@@ -22,11 +24,15 @@ class TestMain(unittest.TestCase):
         self.assertEquals(r.status_code, 200)
         # once user logged out, goes back to the home page and display hello world
         self.assertEquals(r.text, "Hello World!")
-        
-    # Julia's Test
-    def test_home(self):
-        r = requests.get(self.URL'/groups')
-        # status code 200 means we got a response back, user entered group
-        self.assertEquals(r.status_code, 200)
-        # if user isn't in any group, return "You aren't in any groups!"
-        self.assertEquals(r.text, "You aren't in any groups!")
+
+    # Jonathan's Test
+    def test_login(self, mocked_input):
+        # checks if user has already logged in
+        r = requests.get(self.URL)
+        mocked_input.side_effect = ['Jonathan H', 'password123', 'done']
+        result = login(1)
+        self.assertEqual(result, {'Jonathan H', 'password123'})
+        # once user logins, goes back to home page and display hello world
+        self.assertEquals(r.text, "Hello Jonathan H!")
+
+
